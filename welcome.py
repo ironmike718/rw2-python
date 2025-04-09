6925b7eff0ac88e65be77352d45eb798301f8841
ironmike718 <137319205+ironmike718@users.noreply.github.com>
2025-04-09 02:31:36 +0000

    created welcome.py

---
+++ welcome.py
@@ -0,0 +1,26 @@
+# header/footer
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+
+# starts with a blank line
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+"""
+This is a python multi-line comments
+Everything in between the triple " will NOT be read
+I love Python coding
+My wife does Not love Python coding
+"""
+myname = "George"
+
+if myname == "Jaylen":
+    print("My name is Jaylen")
+else:
+    print("My name is Mike")
+
