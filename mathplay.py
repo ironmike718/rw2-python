a124827d3e35763254e7b9f641b1a5518941574b
ironmike718 <137319205+ironmike718@users.noreply.github.com>
2025-04-10 00:46:38 +0000

    doing some python math

---
+++ mathplay.py
@@ -0,0 +1,15 @@
+myname = "Mike"
+
+mymesg = "is the greatest."
+
+print(myname + " " + mymesg)
+
+mynum1 = 9
+
+mynum2 = 7
+
+product = mynum1 * mynum2
+product1 = str(product)
+
+print("The answer is: " + product1)
+
